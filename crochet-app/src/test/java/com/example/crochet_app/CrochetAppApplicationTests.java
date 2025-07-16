c11ab2cc892ab58b01a3c2211b301bf2a98f9276
jennyo <jennifer.oleary.10@gmail.com>
2025-07-16 08:34:31 -0500

    init commit

---
+++ crochet-app/src/test/java/com/example/crochet_app/CrochetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crochet_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
