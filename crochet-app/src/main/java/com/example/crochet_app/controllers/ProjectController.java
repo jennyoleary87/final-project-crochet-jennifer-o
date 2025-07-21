5c38b0487a014be255e3691d9c8fe2f3c3268354
jennyo <jennifer.oleary.10@gmail.com>
2025-07-21 15:26:31 -0500

    create frontend in VS Code: npm install react-router-dom

--- crochet-app/src/main/java/com/example/crochet_app/controllers/ProjectController.java
+++ crochet-app/src/main/java/com/example/crochet_app/controllers/ProjectController.java
@@ -1,6 +1,7 @@
 package com.example.crochet_app.controllers;
 
 import com.example.crochet_app.models.Project;
+import com.example.crochet_app.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -11,5 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectController {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired // initialize constructor
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping("") // get full list of projects
+    public ResponseEntity<?> getAllProjects() {
+        List<Project> allProjects = projectRepository.findAll();
+        return new ResponseEntity<>(allProjects, HttpStatus.OK);
+    }
+
+
 
 }
--- crochet-app/src/main/java/com/example/crochet_app/controllers/UserController.java
+++ crochet-app/src/main/java/com/example/crochet_app/controllers/UserController.java
@@ -1,4 +1,16 @@
 package com.example.crochet_app.controllers;
 
+import com.example.crochet_app.models.User;
+import com.example.crochet_app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 }
