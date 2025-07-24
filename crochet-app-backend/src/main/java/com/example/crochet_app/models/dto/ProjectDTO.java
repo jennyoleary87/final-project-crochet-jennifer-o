a4190061556292465a514feb6b6103e5098e326f
jennyo <jennifer.oleary.10@gmail.com>
2025-07-24 12:21:36 -0500

    create DTOs

---
+++ crochet-app-backend/src/main/java/com/example/crochet_app/models/dto/ProjectDTO.java
@@ -0,0 +1,33 @@
+package com.example.crochet_app.models.dto;
+
+import com.example.crochet_app.models.Project;
+import com.example.crochet_app.models.ProjectDetails;
+
+public class ProjectDTO {
+
+    private String name;
+    private int userId;
+    private ProjectDetails details;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public ProjectDetails getDetails() {
+        return details;
+    }
+
+    public void setDetails(ProjectDetails details) {
+        this.details = details;
+    }
+}
---
+++ crochet-app-backend/src/main/java/com/example/crochet_app/models/dto/UserDTO.java
@@ -0,0 +1,33 @@
+package com.example.crochet_app.models.dto;
+
+public class UserDTO {
+    private String username;
+    private String email;
+    private String password;
+
+    public UserDTO() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
