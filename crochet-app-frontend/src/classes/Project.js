dd1f3c046c5dddef4d4e11cd321afb7e6a80845b
jennyo <jennifer.oleary.10@gmail.com>
2025-07-27 07:01:28 -0500

    initialize classes

---
+++ crochet-app-frontend/src/classes/Project.js
@@ -0,0 +1,8 @@
+export default class Project {
+    constructor(id, name, details, user) {
+        this.id = id;
+        this.name = name;
+        this.details = details;
+        this.user = user;
+    }
+}
---
+++ crochet-app-frontend/src/classes/ProjectDetails.js
@@ -0,0 +1,9 @@
+export default class ProjectDetails {
+    constructor(id, description, timeSpent, yarn, hook) {
+        this.id = id;
+        this.description = description;
+        this.timeSpent = timeSpent;
+        this.yarn = yarn;
+        this.hook = hook;
+    }
+}
