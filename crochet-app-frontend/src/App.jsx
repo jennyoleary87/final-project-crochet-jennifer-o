a6e8f36952de0a147b4265b64b507cb88a0aaeaa
jennyo <jennifer.oleary.10@gmail.com>
2025-08-04 02:04:34 -0500

    app routing

--- crochet-app-frontend/src/App.jsx
+++ crochet-app-frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProjectsPage from './components/ProjectsPage';
+import ProjectDetails from './components/ProjectDetails';
+import Header from './components/Header';
+import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          {/* <Route path="/" element={<>} /> */}
-        </Routes>
-      </HashRouter>
+      <Router>
+        <div>
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/projects/:projectId" element={<ProjectDetails />} />
+              <Route path="*" element={<div>Not Found</div>} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
