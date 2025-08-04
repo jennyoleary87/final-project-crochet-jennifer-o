b0c1f5b769cd32eb036da18e514e159c6e1bfdf4
jennyo <jennifer.oleary.10@gmail.com>
2025-08-04 18:33:56 -0500

    fix some styling

--- crochet-app-frontend/src/App.jsx
+++ crochet-app-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import ProjectDetails from './components/ProjectDetails';
 import Header from './components/Header';
 import './App.css';
 import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Header />
           <main className="main-content">
             <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
               <Route path="/projects" element={<ProjectsPage />} />
               <Route path="/projects/:projectId" element={<ProjectDetails />} />
               <Route path="*" element={<div>Not Found</div>} />
